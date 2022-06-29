3a92b7fb1d3e3e80897e1d862c85b003d91bb653
Lukas Würzburger <lukas.w@me.com>
2022-06-29 13:47:55 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function loadJSONFile(file, callback) {
+	var client = new XMLHttpRequest();
+	client.open('GET', file);
+	client.onreadystatechange = function() {
+		if (client.readyState === XMLHttpRequest.DONE) {
+		  	callback(JSON.parse(client.responseText));
+	    }
+	}
+	client.send();
+}
+
+function loadClips() {
+	loadJSONFile('./static-data/clips.json', function(content) {
+		renderClips(content)
+	})
+}
+
+function renderClips(clips) {
+	for (var i = 0; i <= clips.length - 1; i++) {
+		renderClip(clips[i])
+	}	
+}
+
+function renderClip(clip) {
+	const path = './videos/'
+	const fileName = clip.video_file
+	const html = '<video width="250" height="250" controls><source src="' 
+		+ fileName
+		+ '" type="video/mp4" /></video>'
+	document.getElementById("clips").innerHTML += html
+}
